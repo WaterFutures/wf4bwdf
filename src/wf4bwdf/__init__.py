06934af6aa652f90134ae5639ed71ac4c6f320e1
Dennis <dennis.zanutto@polimi.it>
2025-09-17 22:09:00 +0200

    data loading functions

--- src/wf4bwdf/__init__.py
+++ src/wf4bwdf/__init__.py
@@ -1,4 +1,8 @@
-from . import _data_loading
+from ._data_loading import load_complete_dataset, load_iteration_dataset
 
 def hello() -> str:
+    print("Complete dataset:")
+    print(load_complete_dataset())
+    print("Filtered iter 1:")
+    print(load_iteration_dataset(1, use_letters_for_names=True))
     return "Hello from WF 4 BWDF!"
